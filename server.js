4cc81cde83061c3f7fe855f0725e2895990b323a
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-08-31 17:18:21 +0330

    db connection.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+require("./config/dbConnect");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT , console.log(`server is runing on ${PORT}`))
-
-
+app.listen(PORT, console.log(`server is runing on ${PORT}`));
