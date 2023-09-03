fe9db3ac36dfaef6f1950094b57f3d5c38913387
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-09-03 16:19:44 +0330

    added comment and category routes.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const userRouter = require("./routes/users/userRoutes");
+const postRoutes = require("./routes/posts/postRoutes");
+const commentRoutes = require("./routes/comments/commentRoutes");
+const categoryRoutes = require("./routes/categories/categoryRoutes");
 const dotenv = require("dotenv").config();
 require("./config/dbConnect");
 
@@ -7,9 +10,25 @@ const app = express();
 
 //------------
 // user Routes
-app.use('/api/v1/users',userRouter)
 
+app.use("/api/v1/users", userRouter);
+
+//------------
+// post Routes
+
+app.use("/api/v1/posts", postRoutes);
+
+//------------
+// comment Routes
+
+app.use("/api/v1/comments", commentRoutes);
+
+//------------
+// category Routes
+
+app.use("/api/v1/categorys", categoryRoutes);
+
+//------------
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, console.log(`server is runing on ${PORT}`));
