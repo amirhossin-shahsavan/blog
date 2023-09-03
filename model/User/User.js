7a8e9da16f6c09568262d3cb02c330fbe814cdde
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-09-03 12:16:24 +0330

    create user model and finish.

---
+++ model/User/User.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: [true, "First Name is required"],
+  },
+  Lastname: {
+    type: String,
+    required: [true, "Last Name is required"],
+  },
+  profilephoto: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password Name is required"],
+  },
+  postCount: {
+    type: Number,
+    default: 0,
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: String,
+    enum: ["Admin", "Guest", "Editor"],
+  },
+  viewedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  followers: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  following: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  posts:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  }
+});
