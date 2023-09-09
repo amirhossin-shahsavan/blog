8181dda06101eedc4dedd626479745be74f4ca53
amirhossin-shahsavan <amirhosinshahsavan@gmail.com>
2023-09-09 16:22:54 +0330

    adding global error handler and not found middlewares.

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const userRouter = require("./routes/users/userRoutes");
 const postRoutes = require("./routes/posts/postRoutes");
 const commentRoutes = require("./routes/comments/commentRoutes");
 const categoryRoutes = require("./routes/categories/categoryRoutes");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
 const dotenv = require("dotenv").config();
 require("./config/dbConnect");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
 //------------
 // user Routes
 
@@ -29,6 +31,17 @@ app.use("/api/v1/comments", commentRoutes);
 app.use("/api/v1/categorys", categoryRoutes);
 
 //------------
+// Error handeler middleware
+
+app.use(globalErrorHandler);
+
+// not found error
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    message: `${req.originalUrl} - Route Not Found`,
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`server is runing on ${PORT}`));
